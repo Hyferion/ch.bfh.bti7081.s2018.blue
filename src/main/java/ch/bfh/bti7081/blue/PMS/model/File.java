22caf7bee99227e9ab32c7a785bb2c043c5a82bd
Silas Stulz <sst@studer-raimann.ch>
2018-05-28 20:04:21 +0200

    deleted useless stuff

--- src/main/java/ch/bfh/bti7081/blue/PMS/model/File.java
+++ src/main/java/ch/bfh/bti7081/blue/PMS/model/File.java
@@ -1,4 +1,4 @@
-package ch.bfh.bti7081.blue.PMS;
+package ch.bfh.bti7081.blue.PMS.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
--- src/main/java/ch/bfh/bti7081/blue/PMS/model/Task.java
+++ src/main/java/ch/bfh/bti7081/blue/PMS/model/Task.java
@@ -1,4 +1,4 @@
-package ch.bfh.bti7081.blue.PMS;
+package ch.bfh.bti7081.blue.PMS.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
