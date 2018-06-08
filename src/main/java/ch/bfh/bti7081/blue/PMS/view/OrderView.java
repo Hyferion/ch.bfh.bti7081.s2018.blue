d9ea38c9fd20f76231c562c8f07376b63d9b5276
herrs2 <37441065+herrs2@users.noreply.github.com>
2018-06-08 13:24:57 +0200

    Wahrscheinlichkeit für verfügbar ist höher gesetzt

--- src/main/java/ch/bfh/bti7081/blue/PMS/view/OrderView.java
+++ src/main/java/ch/bfh/bti7081/blue/PMS/view/OrderView.java
@@ -108,7 +108,7 @@ public class OrderView extends CustomComponent implements OrderViewInterface, Cl
 							.setLOGINACCOUNT_USERNAME(UI.getCurrent().getSession().getAttribute("user").toString());
 					orderModelWrite.setName(orderModel.get(i).getName());
 					orderModelWrite.setDate(new Date().toLocaleString());
-					String[] status = {"Verfügbar", "Verfügbar", "In Bearbeitung", "Abgelehnt"};
+					String[] status = {"Verfügbar", "Verfügbar", "In Bearbeitung", "Abgelehnt", "Verfügbar"};
 			         Random random = new Random();
 			         int select = random.nextInt(status.length);
 					orderModelWrite.setStatus(status[select]);
