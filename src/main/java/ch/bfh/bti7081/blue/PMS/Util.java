e52dbebf744968af4d99ac158c82413b1e283587
Fabio Caggiano <jmpout@riseup.net>
2018-06-14 11:20:18 +0200

    hashing function added

--- src/main/java/ch/bfh/bti7081/blue/PMS/Util.java
+++ src/main/java/ch/bfh/bti7081/blue/PMS/Util.java
@@ -1,5 +1,9 @@
 package ch.bfh.bti7081.blue.PMS;
 
+import java.security.MessageDigest;
+
+import javax.xml.bind.DatatypeConverter;
+
 import ch.bfh.bti7081.blue.PMS.model.LoginAccount;
 
 public class Util {
@@ -14,9 +18,22 @@ public class Util {
 
 	// validate password for login
 	public static boolean validatePassword(LoginAccount loginAccount, String loginPassword) {
-	if (loginPassword.equals(loginAccount.getPassword())) {
+	if (createSHA256(loginPassword).equals(loginAccount.getPassword())) {
 		return true;
 	}
 	return false;
+	}
+	
+	private static String createSHA256(String s) {
+		String result = null;
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(s.getBytes("UTF-8"));
+			return DatatypeConverter.printHexBinary(hash);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+		return result;
 	}
 }
